63d3a8c820c1ab8098aaa2429fae1d569ab73134
avgre <avtar.g10@gmail.com>
2020-09-30 12:02:54 -0400

    client-side only suspense fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,20 @@ import Instructions from "../components/instructions"
 import Contact from "../components/contact"
 import OurStory from "../components/ourstory"
 import SEO from "../components/seo"
-const PhoneSlide = React.lazy(() => import("../components/phoneslide"))
 
+const PhoneSlide = React.lazy(() => import("../components/phoneslide"))
 export default function Home() {
+  const isSSR = typeof window === "undefined"
   return (
     <>
       <SEO title="Home" />
       <Layout>
         <About />
-        <Suspense fallback={<div />}>
-          <PhoneSlide />
-        </Suspense>
+        {!isSSR && (
+          <React.Suspense fallback={<div />}>
+            <PhoneSlide />
+          </React.Suspense>
+        )}
         <Instructions />
         <OurStory />
         <Contact />
